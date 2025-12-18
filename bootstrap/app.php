3dee67d89608df5f183156135048e122fedd9fe7
catriel <catrielmichelle5@gmail.com>
2025-12-18 15:54:49 +0100

    correction d'erreur d'hebergement 3

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,9 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
     health: '/up',
 )
 
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
-    })
+   ->withMiddleware(function (Middleware $middleware): void {
+    $middleware->validateCsrfTokens(except: [
+        'api/*',
+    ]);
+})
+
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
